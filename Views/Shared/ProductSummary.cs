d9265e07b75c2e768efa480e0557b7c73dbd695d
Colton Pearce <cpearce3@byu.edu>
2023-02-28 13:24:43 -0700

    CSS Changes

---
+++ Views/Shared/ProductSummary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_cpearce3.Views.Shared
+{
+    public class ProductSummary
+    {
+    }
+}
